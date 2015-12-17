e0ce616d69200a5e3ea9d80303599c5073c484ac
fcboucher <flofloboucher@gmail.com>
2015-12-17 10:35:41 +0100

    Update Example.R

--- Example.R
+++ Example.R
@@ -5,7 +5,7 @@ library(ape)
 library(geiger) # geiger is needed for simulating the tree
 tree=sim.bdtree(stop='taxa',n=20) # tree with 20 tips
 tree$edge.length=100*tree$edge.length/max(branching.times(tree)) # rescale the tree to  a total depth of 100
-TRAIT= Sim_BBM(tree,x0=0,Npts=100,sigma=1,bounds=c(-5, 5)) # TRAIT simulated on the tree, with many hits on the bounds
+TRAIT= Sim_BBM(tree,x0=0,Npts=100,sigma=1,bounds=c(-5, 5)) # TRAIT simulated on the tree, with many hits on the bounds: for that you need to source the function 'Sim_BBM.R'
 
 # Fit the model to the data simulated
 BBM=fit_BBM_model_uncertainty(tree,trait=TRAIT,Npts=100,bounds='Fixed',uncertainty=F) # bounds fixed to min/max of the observed trait, no assessment of uncertainty
